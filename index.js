11d6e224fd66f51c44fde68d4b9ffb5fdcf9f9db
amxwesley <wes85melis@gmail.com>
2019-04-12 23:18:06 -0300

    config

---
+++ index.js
@@ -0,0 +1,7 @@
+/* tem nota de 2, 5 , 10 e 20 */
+
+var valor = 60;
+var valorPg = 100;
+var troco = 0; 
+
+    troco = valorPg - valor;
---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+/* tem nota de 2, 5 , 10 ,20 e 50 disponíveis */
+
+var valor = 60;
+var valorPg = 100;
+var troco = valorPg - valor;
+var notas = [2 , 5 , 10 , 20, 50];
