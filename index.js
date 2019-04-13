0ba0376a381ac4f86802c44a4e1e4b70384d7110
amxwesley <wes85melis@gmail.com>
2019-04-12 23:51:10 -0300

    ops com strings

--- index.js
+++
@@ -1,7 +0,0 @@
-/* tem nota de 2, 5 , 10 e 20 */
-
-var valor = 60;
-var valorPg = 100;
-var troco = 0; 
-
-    troco = valorPg - valor;
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,4 @@
-/* tem nota de 2, 5 , 10 ,20 e 50 disponíveis */
+var nome = "wesley Melis";
+var sobrenome = "Ameixoeira";
+nome += sobrenome;
 
-var valor = 60;
-var valorPg = 100;
-var troco = valorPg - valor;
-var notas = [2 , 5 , 10 , 20, 50];
